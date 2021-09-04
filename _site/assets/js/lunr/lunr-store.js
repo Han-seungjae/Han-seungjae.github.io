0eb6f2ed2947ff3be3771d5ffc70d7f18c3f569e
Han-seungjae <tom7951@daum.net>
2021-09-04 20:23:07 +0900

    comment

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "마이크 테스트",
+        "excerpt":"안녕 세계!  ","categories": ["test"],
+        "tags": ["test"],
+        "url": "/test/mictest/",
+        "teaser": null
+      },{
+        "title": "마이크 테스트",
+        "excerpt":"안녕 세계!  ","categories": ["test"],
+        "tags": ["test"],
+        "url": "/test/ppap/",
+        "teaser": null
+      }]
